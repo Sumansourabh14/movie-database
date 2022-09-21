e348e4d42ae19867675cb439af91cff6a6fde5dd
Suman Sourabh <sumsourabh14@gmail.com>
2022-09-21 23:37:10 +0530

    add movies

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import { default as Navbar } from './Navbar';
 import { default as Main } from './Main';
+import { default as Row } from './Row';
 
-export { Navbar, Main };
+export { Navbar, Main, Row };
