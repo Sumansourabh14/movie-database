1e5db2745119d331df98eda5e0086783326f7923
Suman Sourabh <sumsourabh14@gmail.com>
2022-09-27 12:48:03 +0530

    add movie details

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,8 +1,22 @@
+import { FaStar } from "react-icons/fa";
+
 const Review = ({ review }) => {
     return (
-        <li>
-            <h4>{review?.author}</h4>
-            <p className="text-sm my-4">{review?.content?.slice(0, 1000)}...</p>
+        <li className="flex items-start pb-16">
+            <img
+                src={review?.author_details?.avatar_path?.slice(1)}
+                alt={review?.author}
+                className="rounded-full mr-4"
+            />
+            <div>
+                <h4>{review?.author}</h4>
+                <h5 className="text-sm my-2 text-stone-400">@{review?.author_details?.username}</h5>
+                <div className="flex items-center text-xl">
+                    <h6 className="mr-2">{review?.author_details?.rating}</h6>
+                    <h6><FaStar /></h6>
+                </div>
+            </div>
+            <p className="text-sm ml-8">{review?.content?.slice(0, 1000)}...</p>
         </li>
     )
 }
